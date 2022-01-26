98ae8958a30ea5b129dea537fb3b4b658ed5e8c1
Astro <akomikron@gmail.com>
2022-01-26 23:34:07 +0200

    merge
    cleanup

---
+++ royalib/ext/json/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Jim/source/repos/royalty/royalib/ext/json
+# Build directory: C:/Users/Jim/source/repos/royalty/royalib/ext/json
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("example")
+subdirs("test")
