997eb4500e9e1f62fba7615a6b59099332142009
Astro <akomikron@gmail.com>
2022-02-09 19:38:54 +0200

    royalty

--- royalty/src/api.h
+++ royalty/src/api.h
@@ -6,17 +6,6 @@
 
 #include <royalib.h>
 
-#define min(a,b)			(((a)<(b))?(a):(b))
-#define max(a,b)			(((a)>(b))?(a):(b))
-
-#include <objidl.h>
-#include <gdiplus.h>
-
-#undef min
-#undef max
-
-#pragma comment (lib,"Gdiplus.lib")
-
 class GlobalWindow : public wl::window_main {
 	GPU_Context *ctx;
 	Proctor *dummy;
